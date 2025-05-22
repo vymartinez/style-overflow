2ba914368dfffe6569235d4e13c129c03714683e
Victor Martinez <victoryanmartinez@gmail.com>
2025-05-22 11:11:14 -0300

    :wrench: chore: inicialização do repositório

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module br.com.styleoverflow.styleoverflow {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens br.com.styleoverflow.styleoverflow to javafx.fxml;
+    exports br.com.styleoverflow.styleoverflow;
+}

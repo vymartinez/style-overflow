bd8278f4d63770f07f68139993544816e2720aa8
Victor Martinez <victoryanmartinez@gmail.com>
2025-05-29 15:07:40 -0300

    :lipstick: style: configuração e adição e estilos

---
+++ src/main/java/br/com/styleoverflow/styleoverflow/App.java
@@ -0,0 +1,26 @@
+package br.com.styleoverflow.styleoverflow;
+
+import br.com.styleoverflow.styleoverflow.screens.LoginAndRegister;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        Parent root = LoginAndRegister.showLogin(stage);
+        Scene scene = new Scene(root);
+        scene.getStylesheets().addAll(
+                getClass().getResource("/css/style.css").toExternalForm()
+        );
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.setTitle("Style Overflow");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

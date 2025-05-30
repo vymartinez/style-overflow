b8c7ce238df9713d1adc00777a677a1dabd37a34
Victor Martinez <victoryanmartinez@gmail.com>
2025-05-30 11:40:43 -0300

    :books: docs: adição de arquivos de criação de dados

---
+++ seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO users (name, email, password, role, gender, cpf, address, cep, cellphone) VALUES ("administrador", "admin@gmail.com", "123456", "ADMIN", "MALE", "12345678910", "Estrutural Rua 9", "12345678", "61912345678");
+
+INSERT INTO products (name, size, gender, color, stock, price) VALUES
+("brusinha", "PP", "FEMALE", "rosa", "1", "25.99"),
+("brusinha", "P", "FEMALE", "rosa", "0", "25.99"),
+("brusinha", "M", "FEMALE", "rosa", "6", "25.99"),
+("brusinha", "G", "FEMALE", "rosa", "12", "25.99"),
+("brusinha", "GG", "FEMALE", "rosa", "15", "25.99"),
+("casaco", "P", "MALE", "preto", "12", "69.99"),
+("casaco", "M", "MALE", "preto", "6", "69.99"),
+("casaco", "G", "MALE", "preto", "2", "69.99"),
+("casaco", "GG", "MALE", "preto", "3", "69.99"),
+("regatinha", "P", "MALE", "branca", "15", "33.99"),
+("regatinha", "M", "MALE", "branca", "0", "33.99"),
+("regatinha", "G", "MALE", "branca", "6", "33.99"),
+("regatinha", "GG", "MALE", "branca", "0", "33.99");
